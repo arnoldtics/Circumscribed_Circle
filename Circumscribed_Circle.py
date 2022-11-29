928799991bf7775c228dec877f84f579d49bf938
Arnoldo Sánchez <arfersan20@gmail.com>
2022-11-29 07:47:36 -0600

    Creando función para generar y validar puntos

--- Circumscribed_Circle.py
+++ Circumscribed_Circle.py
@@ -6,3 +6,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random as r
+
+def createPoints():
+    while True:
+        p1, p2, p3 = (r.randint(-50, 50), r.randint(-50, 50)), (r.randint(-50, 50), r.randint(-50, 50)), (r.randint(-50, 50), r.randint(-50, 50))
+        if p1 == p2 or p2 == p3 or p1 == p3:
+            pass
+        elif p1[0] == p2[0] and p2[0] == p3[0]:
+            pass
+        elif p1[1] == p2[1] and p2[1] == p3[1]:
+            pass
+        else:
+            break
+    return p1, p2, p3
+
+points = createPoints()
+print(points)
